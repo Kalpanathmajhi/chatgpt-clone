e12077f47b757ebfa5e6f83aa5fe050a28b640c7
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2023-06-06 21:56:21 +0530

    api added

--- src/component/chat.js
+++ src/component/chat.js
@@ -1,15 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './chat.css';
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
 
-  const sendMessage = (event) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'POST',
+        url: 'https://alchemytextserg-osipchukv1.p.rapidapi.com/deleteSingleModel',
+        headers: {
+          'X-RapidAPI-Key': '994ad9ecfamsh713c9fd6571a367p162aaejsne61772635e87',
+          'X-RapidAPI-Host': 'AlchemyTextserg-osipchukV1.p.rapidapi.com'
+        }
+      };
+    
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const sendMessage = async (event) => {
     event.preventDefault();
-    setMessages([...messages, { text: input, isUser: true }]);
-    setInput("");
+    if(input.trim() !== "") {
+      setMessages([...messages, { text: input, isUser: true }]);
+      // Send the user's message to your API
+      const options = {
+        method: 'POST',
+        url: 'https://alchemytextserg-osipchukv1.p.rapidapi.com/your-api-endpoint', // Replace with the correct endpoint
+        headers: {
+          'X-RapidAPI-Key': '994ad9ecfamsh713c9fd6571a367p162aaejsne61772635e87',
+          'X-RapidAPI-Host': 'AlchemyTextserg-osipchukV1.p.rapidapi.com'
+        },
+        data: {
+          message: input
+        }
+      };
+      try {
+        const response = await axios.request(options);
+        // Add the API's response as a new message
+        setMessages(prevMessages => [...prevMessages, { text: response.data, isUser: false }]);
+      } catch (error) {
+        console.error(error);
+      }
+      setInput("");
+    }
   };
+  
 
   return (
     <div className="chat">
